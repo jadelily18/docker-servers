2f651858947e1d7f49ab712d60bb30ab78e52558
Jade Nash <jade.nashh18@gmail.com>
2022-09-26 11:04:30 -0500

    update install and install_template scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python3 -m pip install requests inquirer wget
+sudo apt -y install python3-pip
+
+pip install requests inquirer wget
 
 curl https://raw.githubusercontent.com/jadelily18/docker-servers/main/install_template.py | python3 -
 
