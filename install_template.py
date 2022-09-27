c42149b15108b71bca0f038fdbb4e27bd2da3959
Jade Nash <jade.nashh18@gmail.com>
2022-09-26 20:17:51 -0500

    switch from home to current directory

--- install_template.py
+++ install_template.py
@@ -19,10 +19,10 @@ repo_name = inquirer.select(
 docker_compose_url = requests.get(
     f"https://api.github.com/repos/jadelily18/docker-servers/contents/docker-compose/{repo_name}/docker-compose.yml").json()["download_url"]
 
-if os.path.exists(os.path.join(os.path.expanduser("~"), server_dir_name, "docker-compose.yml")):
+if os.path.exists(os.path.join(os.path.curdir, server_dir_name, "docker-compose.yml")):
     print("\nDirectory already exists! Please retry with a different directory name.")
 else:
-    os.mkdir(os.path.join(os.path.expanduser("~"), server_dir_name))
+    os.mkdir(os.path.join(os.path.curdir, server_dir_name))
     wget.download(docker_compose_url, out=os.path.join(
-        os.path.expanduser("~"), server_dir_name, "docker-compose.yml"))
+        os.path.curdir, server_dir_name, "docker-compose.yml"))
     print("\n\nYou can now run your server using `docker compose up`. If you don't already have docker installed, you can use its their convenience script at https://github.com/docker/docker-install.\n")
